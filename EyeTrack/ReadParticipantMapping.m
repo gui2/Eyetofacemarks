d095f864de45d960b5bc751294a2e764471bc059
gui2 <Gui@guido-pusiols-MacBook-Pro.local>
2014-03-12 20:56:03 -0700

    updates

---
+++ EyeTrack/ReadParticipantMapping.m
@@ -0,0 +1,17 @@
+clear all
+close all
+
+fname ='subjects.csv';
+load mapping.mat;
+inputs = repmat('%s',1,13);
+[A B	C   D   E   F  G  H  I  J  K  L  O ]	= ...     
+  textread(fname,inputs,'delimiter',',','emptyvalue',NaN,'headerlines',1);
+
+for ( i=1:length(C))
+    for ( j=1:length(wwd(:,4)))
+    if (strcmp(C(i),wwd{j,4}))
+        wwd{j,5}=B(i)
+    end;
+  %  disp(
+end;
+end;
