f9b7e68c47e66b6b42f6b66c23ca158d7d7902f1
gui2 <Gui@dn0a223951.sunet>
2014-03-20 13:03:04 -0700

    De-comment functional chunks

--- EyeTrack/PlotGraphsFromMats.m
+++ EyeTrack/PlotGraphsFromMats.m
@@ -1,106 +1,106 @@
-% clear all;
-% close all
-%  load mapping.mat;
-% directoryin ='PartialDistances/'; 
-%  ims = getAllFiles(directoryin);
-% 
-%  iptsetpref('ImshowBorder','tight'); 
-% 
-%  
-%   %% PLOT XF_MALE
-% Accum_sputnik(1:11,1:3) =0;
-% Groups_sputnik(1:11,1:3) =0;
-% s = struct('a',zeros(11,0),'b',zeros(11,0),'c',zeros(11,0));
-% minmax= struct('a',zeros(11,3),'b',zeros(11,3),'c',zeros(11,3));
-% count = struct('a',zeros(1),'b',zeros(1),'c',zeros(1));
-% frames = struct('a',zeros(1),'b',zeros(1),'c',zeros(1));
-% framesX = struct('a',zeros(0),'b',zeros(0),'c',zeros(0));
-% Age = struct('a',zeros(0),'b',zeros(0),'c',zeros(0));
-% IQ = struct('a',zeros(0),'b',zeros(0),'c',zeros(0));
-% 
-%   for o =1:3
-%                      figure;
-%                       for r= 1:length(ims), % for each video    
-%                                            p= char(ims(r,1));
-%                                            [pathstr, name, ext] = fileparts(p);
-%                                            if(~strcmp( '.mat',ext)) 
-%                                                  continue; 
-%                                            end;
-%                                            [pathstr, name, ext] = fileparts(name);
-%                                            double = [name ext];
-%                                            % Split By Groups 
-%                                            for ( j=1:length(wwd(:,5)))
-%                                                         if (~strcmp(double,wwd{j,2}) || ~strcmp('FXS_Males',wwd{j,5})&& o ==3)
-%                                                             continue;
-%                                                         end;
-%                                                         if (~strcmp(double,wwd{j,2}) || ~strcmp('FXS_Females',wwd{j,5})&& o ==2)
-%                                                              continue;
-%                                                         end;
-%                                                          if ((~strcmp(double,wwd{j,2}) || ~strcmp('DD_Participants',wwd{j,5})) && o ==1)
-%                                                              continue;
-%                                                         end;
-% 
-%                                                        mp = load (p);
-%                                                        mp.age = wwd{j,7};
-%                                                        mp.IQ = wwd{j,6};
-%                                                        h = subplot (9,9,r);
-%                                                        ax=get(h,'Position');
-%                                                        ax(1)=ax(1)-0.01; %or wathever 
-%                                                        ax(2)=ax(2)+0.01; %or wathever 
-%                                                        ax(3)=ax(3)+0.02; %or wathever 
-%                                                        set(h,'Position',ax);
-%                                                        axis off;
-%                                                        colors =hsv(12);
-%                                                        hold on; 
-%                                                                         for i = 1:numel(mp.sputnik(:,1))
-%                                                                         bar(i,mp.sputnik(i,1), 'facecolor', colors(i,:));
-%                                                                         end
-%                                                                         hold off;  
-%                                                                         lf =length (mp.ret(:,1));
-%                                                                         switch o
-%                                                                          case 1
-%                                                                           s.a = [s.a,mp.sputnik(:,1)];
-%                                                                           Age.a= [ Age.a,  mp.age];
-%                                                                            IQ.a = [IQ.a,  mp.IQ];
-%                                                                           count.a =count.a+1 ;
-%                                                                         
-%                                                                           frames.a = frames.a + lf ;
-%                                                                           framesX.a = [framesX.a , lf];
-%                                                                           case 2
-%                                                                           s.b =  [s.b,mp.sputnik(:,1)];
-%                                                                          Age.b= [ Age.b,  mp.age];
-%                                                                           IQ.b= [IQ.b,  mp.IQ];
-%                                                                          count.b=count.b+1 ;
-%                                                                          frames.b = frames.b + length (mp.ret(:,1));
-%                                                                          framesX.b = [framesX.b , lf];
-%                                                                            otherwise
-%                                                                          s.c = [s.c,mp.sputnik(:,1)];   
-%                                                                          Age.c= [ Age.c,  mp.age];
-%                                                                          IQ.c= [IQ.c,  mp.IQ];
-%                                                                          count.c=count.c+1 ;
-%                                                                          frames.c= frames.c + length (mp.ret(:,1));
-%                                                                          framesX.c = [framesX.c , lf];
-%                                                                          end
-%                                       end; 
-%                         end;
-%     
-%       %% Setting the title
-%             set(gcf,'NextPlot','add');
-%             axes;
-%             switch o
-%                     case 1
-%                     h = title('DD  Participants');
-%                     case 2
-%                     h = title('FXS Females');
-%                     otherwise
-%                     h = title('FXS  Males');
-%                     end
-%             set(gca,'Visible','off');
-%             set(h,'Visible','on');
-% end;
+clear all;
+close all
+ load mapping.mat;
+directoryin ='PartialDistances/'; 
+ ims = getAllFiles(directoryin);
+
+ iptsetpref('ImshowBorder','tight'); 
+
+ 
+  %% PLOT XF_MALE
+Accum_sputnik(1:11,1:3) =0;
+Groups_sputnik(1:11,1:3) =0;
+s = struct('a',zeros(11,0),'b',zeros(11,0),'c',zeros(11,0));
+minmax= struct('a',zeros(11,3),'b',zeros(11,3),'c',zeros(11,3));
+count = struct('a',zeros(1),'b',zeros(1),'c',zeros(1));
+frames = struct('a',zeros(1),'b',zeros(1),'c',zeros(1));
+framesX = struct('a',zeros(0),'b',zeros(0),'c',zeros(0));
+Age = struct('a',zeros(0),'b',zeros(0),'c',zeros(0));
+IQ = struct('a',zeros(0),'b',zeros(0),'c',zeros(0));
+
+  for o =1:3
+                     figure;
+                      for r= 1:length(ims), % for each video    
+                                           p= char(ims(r,1));
+                                           [pathstr, name, ext] = fileparts(p);
+                                           if(~strcmp( '.mat',ext)) 
+                                                 continue; 
+                                           end;
+                                           [pathstr, name, ext] = fileparts(name);
+                                           double = [name ext];
+                                           % Split By Groups 
+                                           for ( j=1:length(wwd(:,5)))
+                                                        if (~strcmp(double,wwd{j,2}) || ~strcmp('FXS_Males',wwd{j,5})&& o ==3)
+                                                            continue;
+                                                        end;
+                                                        if (~strcmp(double,wwd{j,2}) || ~strcmp('FXS_Females',wwd{j,5})&& o ==2)
+                                                             continue;
+                                                        end;
+                                                         if ((~strcmp(double,wwd{j,2}) || ~strcmp('DD_Participants',wwd{j,5})) && o ==1)
+                                                             continue;
+                                                        end;
+
+                                                       mp = load (p);
+                                                       mp.age = wwd{j,7};
+                                                       mp.IQ = wwd{j,6};
+                                                       h = subplot (9,9,r);
+                                                       ax=get(h,'Position');
+                                                       ax(1)=ax(1)-0.01; %or wathever 
+                                                       ax(2)=ax(2)+0.01; %or wathever 
+                                                       ax(3)=ax(3)+0.02; %or wathever 
+                                                       set(h,'Position',ax);
+                                                       axis off;
+                                                       colors =hsv(12);
+                                                       hold on; 
+                                                                        for i = 1:numel(mp.sputnik(:,1))
+                                                                        bar(i,mp.sputnik(i,1), 'facecolor', colors(i,:));
+                                                                        end
+                                                                        hold off;  
+                                                                        lf =length (mp.ret(:,1));
+                                                                        switch o
+                                                                         case 1
+                                                                          s.a = [s.a,mp.sputnik(:,1)];
+                                                                          Age.a= [ Age.a,  mp.age];
+                                                                           IQ.a = [IQ.a,  mp.IQ];
+                                                                          count.a =count.a+1 ;
+                                                                        
+                                                                          frames.a = frames.a + lf ;
+                                                                          framesX.a = [framesX.a , lf];
+                                                                          case 2
+                                                                          s.b =  [s.b,mp.sputnik(:,1)];
+                                                                         Age.b= [ Age.b,  mp.age];
+                                                                          IQ.b= [IQ.b,  mp.IQ];
+                                                                         count.b=count.b+1 ;
+                                                                         frames.b = frames.b + length (mp.ret(:,1));
+                                                                         framesX.b = [framesX.b , lf];
+                                                                           otherwise
+                                                                         s.c = [s.c,mp.sputnik(:,1)];   
+                                                                         Age.c= [ Age.c,  mp.age];
+                                                                         IQ.c= [IQ.c,  mp.IQ];
+                                                                         count.c=count.c+1 ;
+                                                                         frames.c= frames.c + length (mp.ret(:,1));
+                                                                         framesX.c = [framesX.c , lf];
+                                                                         end
+                                      end; 
+                        end;
+    
+      %% Setting the title
+            set(gcf,'NextPlot','add');
+            axes;
+            switch o
+                    case 1
+                    h = title('DD  Participants');
+                    case 2
+                    h = title('FXS Females');
+                    otherwise
+                    h = title('FXS  Males');
+                    end
+            set(gca,'Visible','off');
+            set(h,'Visible','on');
+end;
 
 
-load 'uptohere.mat';
+%load 'uptohere.mat';
 % Statistics By IQ .
 for o =1:3 % for each group 
     m=figure; 
